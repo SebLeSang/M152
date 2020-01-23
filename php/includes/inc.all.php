b3ea2fac44a9cc584fdd85dffcf98e0b0d3288a9
sebastien.cthbr <sebastien.cthbr@eduge.ch>
2020-01-23 16:07:26 +0100

    Sauvegarde fin de cours 1

---
+++ php/includes/inc.all.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+/**
+ * INCLUDE DE LA CONNEXION A LA BDD
+ */
+require_once $_SERVER['DOCUMENT_ROOT'].'/M152/php/server/database.php';
+/**
+ * INCLUDE DES FONCTIONS
+ */
+require_once $_SERVER['DOCUMENT_ROOT'].'/M152/php/functions.php';
--- post.php
+++ post.php
@@ -11,42 +11,48 @@
 
     <title>Ajouter un post</title>
 </head>
-<nav class="navbar navbar-expand-lg navbar-light bg-primary">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-3" style="width: 50px; height: 50px;">
-            <svg width="100%" height="100%" viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" clip-rule="evenodd"
-                    d="M23 0C10.2975 0 0 10.2975 0 23V233C0 245.703 10.2975 256 23 256H233C245.703 256 256 245.703 256 233V23C256 10.2975 245.703 0 233 0H23ZM165.669 96.6C157.669 92.0667 148.735 89.8 138.869 89.8C126.069 89.8 116.002 93.5333 108.669 101V51.6H70.6687V200H106.869V189.2C113.935 197.6 124.602 201.8 138.869 201.8C148.735 201.8 157.669 199.533 165.669 195C173.802 190.333 180.202 183.733 184.869 175.2C189.669 166.667 192.069 156.8 192.069 145.6C192.069 134.4 189.669 124.6 184.869 116.2C180.202 107.667 173.802 101.133 165.669 96.6ZM147.269 164.8C143.002 169.333 137.535 171.6 130.869 171.6C124.202 171.6 118.735 169.333 114.469 164.8C110.202 160.133 108.069 153.733 108.069 145.6C108.069 137.6 110.202 131.333 114.469 126.8C118.735 122.267 124.202 120 130.869 120C137.535 120 143.002 122.267 147.269 126.8C151.535 131.333 153.669 137.6 153.669 145.6C153.669 153.733 151.535 160.133 147.269 164.8Z"
-                    fill="white" />
-            </svg>
-        </ul>
-        <form class="form-inline mr-5">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
-        </form>
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link text-light" href="index.php">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-light" href="post.php">Post</a>
-            </li>
-        </ul>
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item text-light">user</li>
-        </ul>
-    </div>
-</nav>
-<div class="container mt-5">
-
-</div>
 
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-3" style="width: 50px; height: 50px;">
+                <svg width="100%" height="100%" viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd"
+                        d="M23 0C10.2975 0 0 10.2975 0 23V233C0 245.703 10.2975 256 23 256H233C245.703 256 256 245.703 256 233V23C256 10.2975 245.703 0 233 0H23ZM165.669 96.6C157.669 92.0667 148.735 89.8 138.869 89.8C126.069 89.8 116.002 93.5333 108.669 101V51.6H70.6687V200H106.869V189.2C113.935 197.6 124.602 201.8 138.869 201.8C148.735 201.8 157.669 199.533 165.669 195C173.802 190.333 180.202 183.733 184.869 175.2C189.669 166.667 192.069 156.8 192.069 145.6C192.069 134.4 189.669 124.6 184.869 116.2C180.202 107.667 173.802 101.133 165.669 96.6ZM147.269 164.8C143.002 169.333 137.535 171.6 130.869 171.6C124.202 171.6 118.735 169.333 114.469 164.8C110.202 160.133 108.069 153.733 108.069 145.6C108.069 137.6 110.202 131.333 114.469 126.8C118.735 122.267 124.202 120 130.869 120C137.535 120 143.002 122.267 147.269 126.8C151.535 131.333 153.669 137.6 153.669 145.6C153.669 153.733 151.535 160.133 147.269 164.8Z"
+                        fill="white" />
+                </svg>
+            </ul>
+            <form class="form-inline mr-5">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+            </form>
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link text-light" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="post.php">Post</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item text-light">user</li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container mt-5">
+        <form>
+            <div class="form-group">
+                <label for="exampleFormControlFile1">Example file input</label>
+                <input type="file" class="form-control-file" id="exampleFormControlFile1">
+            </div>
+        </form>
+    </div>
+
 
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
         integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
