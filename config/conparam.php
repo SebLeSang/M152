4e599be139721f895a585d59204e9c9da14628ae
sebastien.cthbr <sebastien.cthbr@eduge.ch>
2020-01-23 15:51:10 +0100

    test du git ignore

---
+++ config/conparam.php
@@ -0,0 +1,16 @@
+<?php
+
+// Le Type de la base de donnée
+define ('EDB_DBTYPE', "mysql");
+// Adresse de l'hôte
+define ('EDB_HOST', "localhost");
+// Numéro de Port
+define ('EDB_PORT', "3306");
+// Nom de la base de donnée
+define ('EDB_DBNAME', "m152");
+// Nom du compte de la base de donnée
+define ('EDB_USER', "root");
+// Mot de passe du compte
+define ('EDB_PASS', "Super");
+
+?>
