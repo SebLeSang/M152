eb34aac3163c93529e56fd53865b7ea19dc0bd33
sebastien.cthbr <sebastien.cthbr@eduge.ch>
2020-02-20 14:59:26 +0100

    Chapite 2 terminé

--- php/functions.php
+++ php/functions.php
@@ -1,18 +1,86 @@
 <?php
-function uploadFile(){
-    $target_dir = $_SERVER['DOCUMENT_ROOT'].'/m152/img_temp/';
-    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-    $uploadOk = 1;
-    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-    // Check if image file is a actual image or fake image
-    if(isset($_POST["submit"])) {
-        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-        if($check !== false) {
-            echo "File is an image - " . $check["mime"] . ".";
-            $uploadOk = 1;
-        } else {
-            echo "File is not an image.";
-            $uploadOk = 0;
+
+/**
+ * Fichier de fonctions
+ */
+require_once $_SERVER['DOCUMENT_ROOT'] . '/m152/php/includes/incAll/inc.all.php';
+
+/**
+ * upload des medias dans la base de données
+ *
+ * @param [string] $mediaType
+ * @param [string] $mediaName
+ * @return [int] l'id du media
+ */
+function uploadMedias($mediaType, $mediaName)
+{
+    $todayDate = date("Y-m-d H:i:s");
+
+    EDatabase::beginTransaction();
+    // insert dans la table media
+    $sql = 'INSERT INTO media (typeMedia, nomMedia, creationDate) VALUES (:mType, :nMedia, :cDate)';
+    try {
+        $stmt = EDatabase::prepare($sql);
+        $stmt->execute(array(
+            ':mType' => $mediaType,
+            ':nMedia' => $mediaName,
+            ':cDate' => $todayDate
+        ));
+    } catch (PDOException $e) {
+        echo "upload error" . $e->getMessage();
+        EDatabase::rollBack();
+        return false;
+    }
+    $id = intval(EDatabase::lastInsertId());
+
+    // si tout va bien
+    EDatabase::commit();
+    return $id;
+}
+
+/**
+ * upload d'un post qui contient des medias
+ *
+ * @param [string] $comment
+ * @return true si l'insert a fonctionné
+ */
+function createPost($comment, $idMediaArray)
+{
+    $todayDate = date("Y-m-d H:i:s");
+
+    EDatabase::beginTransaction();
+    // insert dans la table post
+    $sql = 'INSERT INTO post (commentaire, creationDate) VALUES(:comment, :cDate)';
+    try {
+        $stmt = EDatabase::prepare($sql);
+        $stmt->execute(array(
+            ':comment' => $comment,
+            ':cDate' => $todayDate
+        ));
+    } catch (PDOException $e) {
+        echo "upload error" . $e->getMessage();
+        EDatabase::rollBack();
+        return false;
+    }
+    $idPost = intval(EDatabase::lastInsertId());
+
+    // insert dans la table intermediaire
+    foreach ($idMediaArray as $m) {
+        $sql = 'INSERT INTO contenir (media_idmedia, post_idPost) VALUES (:m, :p)';
+        try {
+            $stmt = EDatabase::prepare($sql);
+            $stmt->execute(array(
+                ':m' => $m,
+                ':p' => $idPost
+            ));
+        } catch (PDOException $e) {
+            echo "upload error" . $e->getMessage();
+            EDatabase::rollBack();
+            return false;
         }
     }
+
+    // si tout va bien
+    EDatabase::commit();
+    return true;
 }
--- php/newPost.php
+++ php/newPost.php
@@ -1,24 +1,27 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'].'/m152/php/includes/incAll/inc.all.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/m152/php/includes/incAll/inc.all.php';
 
 $msg = filter_input(INPUT_POST, 'commentaire', FILTER_SANITIZE_STRING);
 
-
 $total_size = 0;
+$idArr = array();
 if (isset($_FILES) && is_array($_FILES) && count($_FILES) > 0) {
     $files = $_FILES['myFiles'];
-    for($i=0; $i < count($files['name']); $i++){
+    for ($i = 0; $i < count($files['name']); $i++) {
         $total_size += $files['size'][$i];
-        if ($total_size < 70000 || $files['size'][$i] < 3000) {
-            if($files["type"][0] == "image/png" || $files["type"][0] == "image/gif" || $files["type"][0] == "image/jpg"){
+        if ($total_size < 70000000 || $files['size'][$i] < 3000000) {
+            if ($files["type"][0] == "image/png" || $files["type"][0] == "image/gif" || $files["type"][0] == "image/jpg") {
                 $fileName = $files['name'][$i];
-                $newName = uniqid($fileName);
-                move_uploaded_file($files['tmp_name'][$i], '../img_temp/' . $newName);
-            }
-            else
+                $splitName = explode(".", $fileName);
+                $newName = uniqid($splitName[0]);
+                $finalName = $newName . "." . $splitName[1];
+                $id = uploadMedias($files["type"][0], $finalName);
+                array_push($idArr, $id);
+                move_uploaded_file($files['tmp_name'][$i], '../img_temp/' . $finalName);
+            } else
                 echo "le type de fichier n'est pas correct";
-        }
-        else
+        } else
             echo "la taille totale des fichier dépasse 70M ou la taille d'un fichier est supérieur a 3M";
     }
+    createPost($msg, $idArr);
 }
